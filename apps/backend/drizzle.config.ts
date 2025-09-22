8a5608fa1c3db60ed27217f155957ca4c865edf3
elvus <lvssgv@gmail.com>
2025-09-22 09:11:20 -0300

    Refactor database configuration to support both PostgreSQL and SQLite

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { defineConfig } from "drizzle-kit";
+// @ts-expect-error outside dir
+const { DATABASE_TYPE, DATABASE_URL } = process.env;
+
 export default defineConfig({
-  out: "./drizzle",
+  out: `./drizzle-${DATABASE_TYPE}`,
   schema: "./src/db/schema.ts",
-  dialect: "postgresql",
+  dialect: DATABASE_TYPE === "sqlite" ? "sqlite" : "postgresql",
   dbCredentials: {
-    // @ts-expect-error outside dir
-    url: process.env.DATABASE_URL!,
+    url: DATABASE_URL!,
   },
 });
